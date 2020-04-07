252e6916c7a9d1b42c7d1dacacdf8006dd22cd2d
Joseph Reed <jereed559@gmail.com>
2020-04-07 01:22:59 -0700

    hide overflow

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import Navbar from './Navbar'
 import Header from './Header'
 import Particles from 'react-particles-js'
 import { makeStyles } from '@material-ui/styles'
+import { Box } from '@material-ui/core'
+import { ImageSearch } from '@material-ui/icons'
 
 const useStyles = makeStyles({
   particlesCanvas: {
@@ -13,11 +15,11 @@ const useStyles = makeStyles({
 const Home = () => {
   const classes = useStyles()
   return (
-    <div className='benis'>
+    <Box style={{minHeight: '100vw', margin: '0'}} className="boxy">
     <Navbar page="Home"/>
       <Header />
       <Particles canvasClassName={classes.particlesCanvas} />
-    </div>
+    </Box>
   )
 }
 
