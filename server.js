e6a8ffe2b9ca79004b8cefa45f35e37300aadcb3
Joseph Reed <jereed559@gmail.com>
2020-06-09 05:28:37 -0700

    add proxy

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser')
 const app = express();
 // Connect Database
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https')
-  res.redirect(`https://${req.header('host')}${req.url}`);
-  else
-  next()
-  })
-  }
 connectDB();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
