4ad70b612b530059aaa4f25b20f342f4f4284df4
Joseph Reed <jereed559@gmail.com>
2020-06-09 05:12:44 -0700

    remove proxy

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use('/api/contact', require('./routes/api/contact'));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
